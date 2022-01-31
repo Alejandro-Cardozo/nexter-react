8c3e595e0251e98e19b8c143635455d441ef8a1e
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-01-31 10:56:37 -0300

    Giving structure to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div className="container">
+      <div className="sidebar">
+        Sidebar
+      </div>
+
+      <header className="header">
+        Header
       </header>
+
+      <div className="realtors">
+        Top 3 realtors
+      </div>
+
+      <div className="features">
+        Features
+      </div>
+
+      <div className="story__pictures">
+        Story pictures
+      </div>
+
+      <div className="story__content">
+        Story content
+      </div>
+
+      <section className="homes">
+        Homes
+      </section>
+
+      <section className="gallery">
+        Gallery
+      </section>
+
+      <footer className="footer">
+        Footer 
+      </footer>
     </div>
   )
 }
