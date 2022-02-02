e8cef891dd1def3f3e085e73af2cef1077d1e428
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-02-02 15:50:09 -0300

    Formatting features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,48 +12,48 @@ function Features() {
   return (
     <div className='features'>
       <div className='feature'>
-        <Global className='side-nav__icon' />
-        <h4 className='heading-4'>World's best luxury homes</h4>
+        <Global className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>World's best luxury homes</h4>
         <p className='feature_text'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sint a
           porro, beatae accusantium ipsa?
         </p>
       </div>
       <div className='feature'>
-        <Trophy className='side-nav__icon' />
-        <h4 className='heading-4'>Only the best properties</h4>
+        <Trophy className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>Only the best properties</h4>
         <p className='feature_text'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sit
           illo sint qui nihil voluptates.
         </p>
       </div>
       <div className='feature'>
-        <MapPin className='side-nav__icon' />
-        <h4 className='heading-4'>All homes in top locations</h4>
+        <MapPin className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>All homes in top locations</h4>
         <p className='feature_text'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
           blanditiis fugit, fugiat quis labore explicabo!
         </p>
       </div>
       <div className='feature'>
-        <Key className='side-nav__icon' />
-        <h4 className='heading-4'>New Home in one week</h4>
+        <Key className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>New Home in one week</h4>
         <p className='feature_text'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
           nisi. Ea explicabo suscipit ipsam est.
         </p>
       </div>
       <div className='feature'>
-        <Presentation className='side-nav__icon' />
-        <h4 className='heading-4'>Top 1% realtors</h4>
+        <Presentation className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>Top 1% realtors</h4>
         <p className='feature_text'>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, dolore
           doloribus? Explicabo adipisci ipsa quibusdam.
         </p>
       </div>
       <div className='feature'>
-        <Lock className='side-nav__icon' />
-        <h4 className='heading-4'>Secure payments on nexter</h4>
+        <Lock className='feature__icon' />
+        <h4 className='heading-4 heading-4--dark'>Secure payments on nexter</h4>
         <p className='feature_text'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quasi
           impedit deserunt quisquam molestias repudiandae?
